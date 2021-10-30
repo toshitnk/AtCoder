003c9009706bf13cde7894ca4d080c35a88f87f8
{toshitnk} <{toshiya0417tanaka@gmail.com}>
2021-10-31 08:49:13 +0900

    first commit

--- apg4b/hello.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() 
-{
-// 　mainの中がプログラム
-// cout に　<< hoge <<endl を送り終了
-  cout << "Hello, world!" << endl;
-}
--- apg4b/string.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-	string str = "Hello";
-	cout << str.size() << endl;
-	cout << "Hello"s.size() << endl;
-}
-
